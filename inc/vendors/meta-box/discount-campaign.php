1fdb56d969c3b9833ae77132cb9cb311650321ca
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-11-15 12:07:03 +0500

    sdfsdf

--- inc/vendors/meta-box/discount-campaign.php
+++ inc/vendors/meta-box/discount-campaign.php
@@ -17,24 +17,42 @@ if ( !function_exists('themename_register_discount_campaign_meta_boxes') ) {
                     'type'        => 'group',
                     'clone'       => true,
                     'ajax'        => true,
-                    'sort_clone' => true,
+                    'sort_clone'  => true,
                     'group_title' => array( 'field' => 'xptheme_discount_campaign_sale_product' ), // ID of the subfield
                     'fields' => array(
                         array(
                             'id'                => 'product_sale_item',
                             'name'              => esc_html__('Select product sale', 'themename'),
                             'type'              => 'post',
+                            'post_type'         => 'product', // Ensures the field queries WooCommerce products
                             'ajax'              => true,
-                            'query_args'        => themename_get_on_sale_query_args(),
+                            'query_args'        => themename_get_on_sale_query_args(), // Fetches sale products
                             'placeholder'       => esc_html__('Select a product', 'themename'),
                         ),
                     ),
                 ),
             ),
         );
-        
 
         return $meta_boxes;
+    }
+}
+
+if ( !function_exists('themename_get_on_sale_query_args') ) {
+    function themename_get_on_sale_query_args() {
+        return array(
+            'post_type'      => 'product',
+            'post_status'    => 'publish',
+            'posts_per_page' => -1,
+            'meta_query'     => array(
+                array(
+                    'key'     => '_sale_price', // Sale price meta key
+                    'value'   => 0,
+                    'compare' => '>',
+                    'type'    => 'NUMERIC',
+                ),
+            ),
+        );
     }
 }
 
@@ -46,3 +64,17 @@ if ( !function_exists('themename_rem_editor_from_post_type') ) {
         remove_post_type_support( 'tb_discount_campaign', 'editor' );
     }
 }
+
+// Debugging Output (Optional)
+add_action( 'init', function() {
+    $query = new WP_Query( themename_get_on_sale_query_args() );
+    if ( $query->have_posts() ) {
+        while ( $query->have_posts() ) {
+            $query->the_post();
+            error_log( 'Product Found: ' . get_the_title() ); // Logs product titles
+        }
+    } else {
+        error_log( 'No products found on sale' );
+    }
+    wp_reset_postdata();
+});
